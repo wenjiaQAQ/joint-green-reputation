61d73e3acea6db6c1dde2729b10aedc62fc62e8d
CinkerZX <sindy_zhou126@126.com>
2024-08-31 18:46:47 +0200

    Test updateT2G DONE

--- code/updateT2G.m
+++ code/updateT2G.m
@@ -1,18 +1,15 @@
 function updateT2G()
 
-    global strategyPlan;
-    global dynamicT2GUpdate;
-    global n;
+    global strategyPlan dynamicT2GUpdate
 
     % Get the current T2G -> last column of dynamicT2GUpdate
     currentT2GValues = dynamicT2GUpdate(:, end);
 
-    for i = 1:n
-        if strcmp(strategyPlan{i}, '[3, NA]')
-            currentT2GValues(i) = 1;
-        end
+    transferNodes = intersect(find(strategyPlan(:,1) == 3), find(isnan(strategyPlan(:,1))));
+    if ~isempty(transferNodes)
+        currentT2GValues(transferNodes) = 1;
     end
-
+    
     % Add new column to update T2G
     dynamicT2GUpdate = [dynamicT2GUpdate, currentT2GValues];
 end
