0e23d2504f1caf5734b78d5820a2fd63eb24dc79
CinkerZX <sindy_zhou126@126.com>
2024-10-23 08:22:03 +0200

    helperNameGenerator.m main.m DONE. TODO: test all

--- code/helperPlanUpdate.m
+++
@@ -1,4 +0,0 @@
-function plan = helperPlanUpdate(plan, index, value)
-    replacement = repmat(value, length(index), 1);
-    plan(index, :) = replacement;
-end
--- code/showTable.m
+++
@@ -1,5 +0,0 @@
-function showTable(n, matrix)
-    IDs = (1:n)';
-    T = table(IDs, matrix, 'VariableNames', {'ID', 'Value'});
-    disp(T);
-end
