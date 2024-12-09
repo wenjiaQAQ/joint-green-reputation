5399b84322802a4544ae164a98457cf2d09ab549
sindy_zhou126@126.com <sindy_zhou126@126.com>
2024-12-09 15:03:39 +0100

    S-curve Visualization DONE

--- Visualization/S-curve.R
+++ Visualization/S-curve.R
@@ -3,6 +3,8 @@ setwd("../Pre-exp/Data/N100")
 
 # Package
 library(data.table)
+library(dplyr)
+library(latex2exp)
 library(ggplot2)
 library(gridExtra)
 library(readxl)
@@ -50,9 +52,6 @@ T2G_rate <- function(T) { # T is the termination step when N=100, T=50
   write.csv(df_T2G, file = "df_T2G.csv", row.names = FALSE)
 }
 
-## Step 2: Visualization
-
-
 ## helper functions
 get_file_names_helper <- function(keywords){
   list.files(path = '.',
@@ -78,12 +77,11 @@ get_K_helper <- function(fileName){
   return(as.numeric(s)/100)
 }
 
-# Read the origional table, and return a dataframe
-# \| rate 1 | category 3 | time 50 | repeatTime 20
-# \|        | Supplier/Manufacturer/Retailer | 1, 2, ... 51 | 1, 2, ... 20|
-# This dataframe is 1020 * 4
-
 T2G_rate_helper <- function(fileName, T, N){
+  # Read the origional table, and return a dataframe
+  # \| rate 1 | category 3 | time 50 | repeatTime 20
+  # \|        | Supplier/Manufacturer/Retailer | 1, 2, ... 51 | 1, 2, ... 20|
+  # This dataframe is 1020 * 4
   fileName <- sub("\\~\\$", "", fileName)
   myDataframe <- read_excel(fileName, col_names = FALSE)
   
@@ -119,6 +117,49 @@ T2G_rate_helper <- function(fileName, T, N){
   return(T2G_rate)
 }
 
+# Data preparation
 T2G_rate(50)
+
+## Step 2: Visualization
+# Step 2.1 read the data as df and Subset k = 0.25
+df_T2G <- read_csv("../Pre-exp/Data/N100/df_T2G.csv")
+df <- df_T2G[df_T2G$k_rep==0.25, ]
+nrow(df)
+colnames(df) <- c("rate", "role", "timeStep", "expID", "alpha", "k")
+colnames(df)
+
+# Step 2.2 calculate the average rate among the 20 groups of exp
+avg_data <- df %>%
+  group_by(timeStep, role, alpha, k) %>%
+  summarize(ave_rate = mean(rate), .groups = "drop")
+View(avg_data)
+
+# Step 2.2 plot
+alpha_labeller <- function(variable){
+  print(variable[[1]])
+  if (variable[[1]]==0.25 ||variable[[2]]==0.25 ||variable[[3]]==0.25)
+  { return(c(expression(alpha = 0.5),
+              expression(alpha = 0.5),
+              expression(alpha = 0.75)))}
+  return(variable)
+}
+
+df$alpha <- factor(df$alpha,
+                   levels = c(0.25, 0.5, 0.75),
+                   labels=c('alpha==0.25', 'alpha==0.5', 'alpha==0.75'))
+avg_data$alpha <- factor(avg_data$alpha,
+                   levels = c(0.25, 0.5, 0.75),
+                   labels=c('alpha==0.25', 'alpha==0.5', 'alpha==0.75'))
+
+my_plot <- ggplot(df, aes(x = timeStep, y = rate)) +
+  geom_line(aes(group = expID), alpha = 0.3) + # Individual curves with transparency
+  geom_line(data = avg_data,
+            aes(x = timeStep, y = ave_rate),
+            color = 'red',
+            size = 1) + # Ave curves thicker
+  xlim(0, 50)+
+  ylim(0, 1)+
+  labs(x = "Time Step", y = "T2G Rate")+
+  facet_grid(alpha ~ role, labeller = label_parsed)
 
 
